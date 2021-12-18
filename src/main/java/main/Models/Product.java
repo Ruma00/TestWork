ef93bb96299305e727c9ee2d0eab375e988d327c
Ruma00 <hackgen0@gmail.com>
2021-12-18 17:53:44 +0700

    Add db init Add dialect to app.yaml Change version of postgres

--- src/main/java/main/Models/Product.java
+++ src/main/java/main/Models/Product.java
@@ -1,12 +1,10 @@
 package main.Models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
+@Table(name = "products")
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
